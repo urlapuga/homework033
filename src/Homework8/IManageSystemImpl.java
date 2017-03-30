26948a6b00212ae24a6e0a0367ca43d80f16f037
urlapuga <urlapuga@gmail.com>
2017-03-30 20:58:48 +0300

    8

---
+++ src/Homework8/IManageSystemImpl.java
@@ -0,0 +1,52 @@
+package Homework8;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Андрей on 30.03.2017.
+ */
+public class IManageSystemImpl implements IManageSystem {
+
+    private Map<Food, Double> database;
+    @Override
+    public Object save(Object obj, double price) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public Object get(int id) {
+        return null;
+    }
+
+    @Override
+    public Double getPrice(Object obj) {
+        return null;
+    }
+
+    @Override
+    public Set getProducts() {
+        return null;
+    }
+
+    @Override
+    public List<Double> getPrices() {
+        return null;
+    }
+}
