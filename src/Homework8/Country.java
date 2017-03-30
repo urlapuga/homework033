42023b9f8822fd9959f87a8586222c9f55c7a177
urlapuga <urlapuga@gmail.com>
2017-03-30 20:47:05 +0300

    8

---
+++ src/Homework8/Country.java
@@ -0,0 +1,8 @@
+package Homework8;
+
+/**
+ * Created by Андрей on 30.03.2017.
+ */
+public enum Country {
+    UKRAINE,RUSSIA,ENGLAND,POLAND,GERMANY
+}
---
+++ src/Homework8/Main.java
@@ -0,0 +1,14 @@
+package Homework8;
+
+import java.util.*;
+
+import static Homework8.Country.*;
+
+/**
+ * Created by Андрей on 30.03.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(UtilClass.generateId(Country.ENGLAND));
+    }
+}
