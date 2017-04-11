52a1cdb21c22a2476f3bff71f29834749ce1e415
urlapuga <urlapuga@gmail.com>
2017-04-11 21:48:34 +0300

    3.4

--- src/homework41/BankSystemImpl.java
+++ src/homework41/BankSystemImpl.java
@@ -6,21 +6,24 @@ package homework41;
 public class BankSystemImpl implements BankSystem {
     @Override
     public void withdrawOfUser(User user, int amount) {
-
+        user.setBalance(user.getBalance()-amount);
     }
 
     @Override
     public void fundUser(User user, int amount) {
-
+        user.setBalance(user.getBalance()+amount);
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-
+        if ((fromUser.getBalance()-amount)>=0) {
+            fromUser.setBalance(fromUser.getBalance() - amount);
+            toUser.setBalance(toUser.getBalance()+amount);
+        }
     }
 
     @Override
     public void paySalary(User user) {
-
+        user.setBalance(user.getBalance()+user.getSalary());
     }
 }
