95d1880aa9b9cbb199d54ba910fbd43e8566d03d
urlapuga <urlapuga@gmail.com>
2017-05-09 12:56:42 +0300

    iteration added to id generator

--- src/Homework8/UtilClass.java
+++ src/Homework8/UtilClass.java
@@ -4,7 +4,9 @@ package Homework8;
  * Created by Андрей on 30.03.2017.
  */
 public final class UtilClass {
+    static int i = 0;
     public static int generateId(Country country,String name){
-        return country.toString().length()*country.toString().hashCode();
+        i++;
+        return country.toString().length()*country.toString().hashCode()+i;
     }
 }
